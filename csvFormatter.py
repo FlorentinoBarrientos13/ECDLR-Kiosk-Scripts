570845161be0d4e6c68bb12de40e4d1ec6f0e8f2
FlorentinoBarrientos13 <juniorb13@unm.edu>
2019-01-30 12:55:21 -0700

    Update csvFormatter.py
    Added dictionaries and continued on format_string untested

--- csvFormatter.py
+++ csvFormatter.py
@@ -1,24 +1,65 @@
-import csv, os
+import csv
+import os
+
 
 def read_and_create_new_csv(filename):
-    file  = open(filename)
+    file = open(filename)
     csv_file = csv.reader(file)
     array_file = list(csv_file)
+    new_csv = format_string(array_file)
+    
+   
 
 
 def format_string(array_file):
     row = len(array_file)
-    col_delimiter  = [1 ,3, 4]
+    col = len(array_file[0])
+    col_delimiter = [0, 2, 3]
+    boolean_cols = [5, 6, 16]
 
-    for i in row:
+    for i in range(row):
+        for j in range(col):
 
-        for j in col:
-            if not j in col_delimiter:
-                new_row.append(array_file[i][j])
-                csv.write
+            if j > 1:
+                if not j in col_delimiter:
+                    if j >= 17 and j <= 41 and array_file[i][j] == 1:
+                        new_row.append(array_file[i][0])
 
-        new_row = []        
+                    elif j in boolean_cols:
+                        if array_file[i][j] == 1:
+                            new_row.append('Yes')
+                        else:
+                            new_row.append('No')
+                    elif j == 13:
+                        new_row.append(non_student(array_file[i][j]))
 
+                    elif j == 14:
+                        new_row.append(academic_level(array_file[i][j]))
+
+                    else:
+                        new_row.append(array_file[i][j])
+
+        new_row = []
+    
+
+
+def academic_level(array_item):
+    switcher = {
+    1: "Freshman",
+    2: "Sophmore",
+    3: "Junior",
+    4: "Senior",
+    5: "Graduate/Professional",
+    6: "Non-Degree",
+    7: "Faculty/Staff"
+    }.get(array_item," ")
 
     
+def non_student(array_item):
+    switcher = {
+    1: "High School",
+    2: "Transfer",
+    3: "Communty Member"
+
+    }.get(array_item, " ")
     
