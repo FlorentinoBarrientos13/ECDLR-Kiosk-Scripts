1028125975e6126db501e69182d4f2a5a8c31d94
FlorentinoBarrientos13 <juniorb13@unm.edu>
2019-01-31 14:40:23 -0700

    created main, time to debug

--- csvFormatter.py
+++ csvFormatter.py
@@ -1,21 +1,34 @@
-import csv
-import os
+import os ,csv
+
+
+def main():
+    filename = input("Enter file name")
+    read_and_create_new_csv(filename)
 
 
 def read_and_create_new_csv(filename):
+    os.makedirs('formatted', exist_ok=True)
     file = open(filename)
     csv_file = csv.reader(file)
     array_file = list(csv_file)
-    new_csv = format_string(array_file)
-    
-   
+    new_csv = format_array(array_file)
+    new_csv_file = open(os.path.join('formatted', filename), 'w', newline='')
+    writer = csv.writer(new_csv_file)
+
+    for row in new_csv:
+        writer.writerow(row)
+
+    new_csv_file.close()
 
 
-def format_string(array_file):
+def format_array(array_file):
+
     row = len(array_file)
     col = len(array_file[0])
     col_delimiter = [0, 2, 3]
     boolean_cols = [5, 6, 16]
+    new_row = []
+    new_csv = []
 
     for i in range(row):
         for j in range(col):
@@ -39,27 +52,32 @@ def format_string(array_file):
                     else:
                         new_row.append(array_file[i][j])
 
+        new_csv.append(new_row)
         new_row = []
-    
+
+    return new_csv
 
 
 def academic_level(array_item):
     switcher = {
-    1: "Freshman",
-    2: "Sophmore",
-    3: "Junior",
-    4: "Senior",
-    5: "Graduate/Professional",
-    6: "Non-Degree",
-    7: "Faculty/Staff"
-    }.get(array_item," ")
+        1: "Freshman",
+        2: "Sophmore",
+        3: "Junior",
+        4: "Senior",
+        5: "Graduate/Professional",
+        6: "Non-Degree",
+        7: "Faculty/Staff"
+    }.get(array_item, " ")
 
-    
+
 def non_student(array_item):
     switcher = {
-    1: "High School",
-    2: "Transfer",
-    3: "Communty Member"
+        1: "High School",
+        2: "Transfer",
+        3: "Communty Member"
 
     }.get(array_item, " ")
-    
+
+
+if __name__ == "__main__":
+    main()
