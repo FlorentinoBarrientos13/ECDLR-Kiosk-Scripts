68d717adaac676b387abf8a0abdf9ddbef8cdf16
FlorentinoBarrientos13 <juniorb13@unm.edu>
2019-02-28 17:04:51 -0700

    Added listserv email extractor

---
+++ ListServ.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+def main():
+    filename = input("Enter file name\n")
+    file = open(filename)
+    read = csv.reader(file)
+    arr = list(read)
+    row = len(arr)
+    listServ_col = 15
+    serv = open("listserv.txt", "w")
+  #Check whether the student chose yes to signup for the listserv
+    for rows in range(row):
+        if len(arr[rows][listServ_col]) > 0 and arr[rows][listServ_col + 1] == '1' :
+            serv.write(arr[rows][listServ_col] + "\n")
+    serv.close()
+
+if __name__ == "__main__":
+  main()
+pass
