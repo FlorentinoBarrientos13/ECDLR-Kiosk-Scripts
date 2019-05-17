da21b7de5fcde78bc126993918c8545a2ae3a714
Florentino Barrientos <34225203+FlorentinoBarrientos13@users.noreply.github.com>
2019-05-17 17:15:11 -0600

    Update ListServ.py

--- ListServ.py
+++ ListServ.py
@@ -7,11 +7,11 @@ def main():
     read = csv.reader(file)
     arr = list(read)
     row = len(arr)
-    listServ_col = 15
+    listServ_col = 16
     serv = open("listserv.txt", "w")
   #Check whether the student chose yes to signup for the listserv
     for rows in range(row):
-        if len(arr[rows][listServ_col]) > 0 and arr[rows][listServ_col + 1] == '1' :
+        if len(arr[rows][listServ_col]) > 0 and arr[rows][listServ_col] == '1' :
             serv.write(arr[rows][listServ_col] + ",")
     serv.close()
 
